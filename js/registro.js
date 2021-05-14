467ef602d3e21028d5cd2ea0ec4b892eec119f34
Ana Sofia Beltran <anasobeltran@gmail.com>
2021-05-14 13:21:21 -0500

    agregar funcion del javascrip para toma de datos del registro

--- js/registro.js
+++ js/registro.js
@@ -1,4 +1,17 @@
 function redirect(){
     window.location.replace("loging.html");
   }
-  
+
+function registrarusuario(){
+  var formularioPrincipal = document.form;
+  var correo = document.form.correo;
+  var contraseña = document.form.contraseña;
+  var nombre = document.form.nombre;
+  var apellido = document.form.apellido;
+  var universidad = document.form.universidad;
+  alert("usuario registrado \nnombre: "+nombre.value+
+  "\n apellido: "+apellido.value+
+  "\n universidad: "+universidad.value+
+  "\n correo: "+correo.value+
+  "\n contraseña: "+contraseña.value);
+}
