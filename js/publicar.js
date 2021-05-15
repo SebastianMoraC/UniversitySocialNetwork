fd2e1cd2ca0fa6eee6f73a1b67317ce618a8802c
Ana Sofia Beltran <anasobeltran@gmail.com>
2021-05-14 20:10:03 -0500

    ultimas modificaciones del javascript, tomar datos de los formularios

---
+++ js/publicar.js
@@ -0,0 +1,15 @@
+
+function publicar(){
+  var autor = document.formpublicacion.autor;
+  var enlace = document.formpublicacion.enlace;
+  var texto = document.formpublicacion.texto;
+  var lista = document.getElementById("opciones");
+  var indiceSeleccionado = lista.selectedIndex;
+  var opcionSeleccionada = lista.options[indiceSeleccionado];
+  const imageUploader = document.getElementById("file").files[0];;
+  alert('nombre del autor: '+autor.value+
+  '\n enlace: '+enlace.value+
+  '\n texto: '+texto.value+
+  '\n opcion: '+ opcionSeleccionada.value+
+  '\n imagen: '+ imageUploader)
+}
--- js/registro.js
+++ js/registro.js
@@ -3,15 +3,29 @@ function redirect(){
   }
 
 function registrarusuario(){
-  var formularioPrincipal = document.form;
   var correo = document.form.correo;
   var contraseña = document.form.contraseña;
+  var valcontraseña = document.form.valcontraseña;
   var nombre = document.form.nombre;
   var apellido = document.form.apellido;
   var universidad = document.form.universidad;
-  alert("usuario registrado \nnombre: "+nombre.value+
-  "\n apellido: "+apellido.value+
-  "\n universidad: "+universidad.value+
-  "\n correo: "+correo.value+
-  "\n contraseña: "+contraseña.value);
+  re=/^([\da-z_\.-]+)@utp.edu.co/
+  if (correo.value=='' || contraseña.value=='' || valcontraseña.value=='' || nombre.value=='' || apellido.value=='' || universidad.value==''){
+    alert("falto un campo")
+  }else{
+    if (!re.exec(correo.value)){
+      alert("correo no valido")
+    }else{
+      if (contraseña.value==valcontraseña.value){
+        alert("usuario registrado \nnombre: "+nombre.value+
+        "\n apellido: "+apellido.value+
+        "\n universidad: "+universidad.value+
+        "\n correo: "+correo.value+
+        "\n contraseña: "+contraseña.value);
+      }else{
+          alert("contraseña incorrecta")
+      }
+    }
+  }
+
 }
