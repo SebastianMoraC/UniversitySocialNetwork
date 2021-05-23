94c88f33def9de05ab59ba967b21162dbf6fb0be
Sebastián Mora Carmona <sebastian.mora@utp.edu.co>
2021-05-23 13:13:50 -0500

    Añadí el ajax en estadisticas para que capture el nombre de los temas y el id

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -1,38 +1,54 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Tema 1', 'Tema 2', 'Tema 3', 'Tema 4', 'Tema 5', 'Tema 6'],
-        datasets: [{
-            label: 'Categorias',
-            data: [5, 4, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    stepSize: 1
+$.ajax({
+    url: '../BACK-PHP/consumirAPI_llenasTemas.php',
+    method: 'GET',
+    responseType: 'json',
+}).then(function (data) {
+    datosTemas = JSON.parse(data);
+    cantidadDatos = []
+    nombreTemas = []
+    for(var i=0;i<Object.keys(datosTemas).length;i++){
+        nombreTemas.push(datosTemas[i].nombre_tema)
+        cantidadDatos.push(parseInt(datosTemas[i].id_tema)); //Aquí debe cambiarse el id_tema por la cantidad de datos que le gusta un tema en concreto
+      }
+    var ctx = document.getElementById('myChart').getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: nombreTemas,
+            datasets: [{
+                label: 'Categorias',
+                data: cantidadDatos,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        stepSize: 1
+                    }
                 }
             }
         }
-    }
+    });
+
 });
+
