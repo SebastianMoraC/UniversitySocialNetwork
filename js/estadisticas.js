d31d85a977a6281064d86438a4d945c0cc8489b3
Sebastián Mora Carmona <sebastian.mora@utp.edu.co>
2021-05-23 23:24:35 -0500

    Ya finalicé las estadísticas

--- js/estadisticas.js
+++ js/estadisticas.js
@@ -6,58 +6,90 @@ $.ajax({
     datosTemas = JSON.parse(data);
     cantidadDatos = []
     nombreTemas = []
-    for(var i=0;i<Object.keys(datosTemas).length;i++){
-        nombreTemas.push(datosTemas[i].nombre_tema)
-        cantidadDatos.push(parseInt(datosTemas[i].id_tema)); //Aquí debe cambiarse el id_tema por la cantidad de datos que le gusta un tema en concreto
-      }
-      $.ajax({
-        url: '../BACK-PHP/consumirAPI_usuario.php?temas_interes',
+    for (var i = 0; i < Object.keys(datosTemas).length; i++) {
+        nombreTemas.push(datosTemas[i].nombre_tema);
+    }
+    $.ajax({
+        url: '../BACK-PHP/consumirAPI_Usuario.php?temas_interes',
         method: 'GET',
         responseType: 'json',
     }).then(function (data) {
-        console.log(JSON.parse(data)[0].id_tema_interes_usuario);
-        console.log(JSON.parse(data)[1].id_tema_interes_usuario);
-    });
+        users = JSON.parse(data);
+        usersInterest = []
+        for (var i = 0; i < Object.keys(users).length; i++) {
+            if (users[i].id_tema_interes_usuario.length > 1) {
+                for (var k = 0; k < Object.keys(users[i].id_tema_interes_usuario).length; k++) {
+                    if (users[i].id_tema_interes_usuario[k] != ',') {
+                        usersInterest.push(parseInt(users[i].id_tema_interes_usuario[k]));
+                    }
 
-    var ctx = document.getElementById('myChart').getContext('2d');
+                }
+            }
+            else {
+                usersInterest.push(parseInt(users[i].id_tema_interes_usuario));
+            }
+        }
+        cantidadDatos = count(nombreTemas.length, usersInterest)
+        var ctx = document.getElementById('myChart').getContext('2d');
 
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: nombreTemas,
-            datasets: [{
-                label: 'Categorias',
-                data: cantidadDatos,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    ticks: {
-                        stepSize: 1
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: nombreTemas,
+                datasets: [{
+                    label: 'Categorias',
+                    data: cantidadDatos,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            stepSize: 1
+                        }
                     }
                 }
             }
-        }
-    });
+        });
 
+    });
 });
 
+
+
+function count(totalCategories, interestCategories) {
+    console.log(totalCategories);
+    console.log(interestCategories);
+    countData = []
+    for (var i = 1; i <= totalCategories; i++) {
+        countData.push(parseInt(0));
+
+        for (var k = 0; k < interestCategories.length; k++) {
+            if (interestCategories[k] === i) {
+                countData[i - 1] = countData[i - 1] + 1;
+
+            }
+        }
+
+
+    }
+    return countData;
+}
