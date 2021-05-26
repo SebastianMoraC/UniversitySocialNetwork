cd93f38a151bd6feb9e7757195488cde9934b6f7
Sebastián Mora Carmona <sebastian.mora@utp.edu.co>
2021-05-25 21:55:53 -0500

    Ia añadida, para cambiar de un modo a otro se debe recargar

--- js/publicar.js
+++ js/publicar.js
@@ -1,6 +1,10 @@
-rellenarpublicaciones();
-
-function regresar(){
+function showGeneral(){
+  rellenarpublicaciones();
+}
+function reload(){
+  window.location.replace("foro.html");
+}
+function regresar() {
   window.location.replace("foro.html");
 }
 
@@ -15,8 +19,8 @@ function Img() {
   }
 }
 
-function rellenarpublicaciones() {
 
+function rellenarpublicaciones() {
   $.ajax({
     url: '../BACK-PHP/consumirAPI_Foro.php?post',
     method: 'GET',
@@ -81,6 +85,98 @@ function rellenarpublicaciones() {
   });
 }
 
+function activateIA() {
+  userInSession = verusuario();
+  var divdelete = document.getElementById("publicaciones");
+
+
+  
+  $.ajax({
+    url: '../BACK-PHP/consumirAPI_Foro.php?post',
+    method: 'GET',
+    responseType: 'json',
+  }).then(function (data) {
+
+    var publicaciones = JSON.parse(data);
+    var publicationsToShow = []
+
+    $.ajax({
+      url: '../BACK-PHP/consumirAPI_Usuario.php?usuarios_datos',
+      method: 'GET',
+      responseType: 'json',
+    }).then(function (data) {
+      users = JSON.parse(data);
+      for (var i = 0; i < Object.keys(users).length; i++) {
+        if (users[i].id_usuario == userInSession) {
+          temaUserInSession = users[i].id_tema_interes_usuario;
+        }
+      }
+      for (var i = 0; i < Object.keys(publicaciones).length; i++) {
+        for (var k = 0; k < temaUserInSession.length; k++) {
+          if (publicaciones[i].tema_categoria === temaUserInSession[k]) {
+            publicationsToShow.push(publicaciones[i]);
+
+          }
+        }
+      }
+      var divpublicaciones = document.getElementById("publicaciones");
+      for (var i = 0; i < Object.keys(publicationsToShow).length; i++) {
+        console.log("hello")
+        divpublicaciones.innerHTML = divpublicaciones.innerHTML + `
+      <div class="publication row">
+          <div class="container col-12">
+              <div class="row">
+                  <h2 class="publication_name col-8">${publicationsToShow[i].nombre_post}</h1>
+                  <a class="col-2 publication_date" href="${publicationsToShow[i].enlace_post}">Enlace Asociado</a>
+              </div>
+              <div class="row">
+              <b class="publication_category column">Categoria: ${publicationsToShow[i].tema_categoria}</b>
+              </div>
+
+              <p class="publication_text row">${publicationsToShow[i].descripcion_post}</p>
+              <div class="publication_image row">
+                  <img src="${publicationsToShow[i].ubicacion_foto_post}" class="rounded mx-auto d-block publication_image_item ">
+              </div>
+
+              <b class="publication_author row">Autor: ${publicationsToShow[i].id_usuario}</b>
+
+          </div>
+          <div class="publication_ratio col-12">
+              <i class="far fa-star fa-2x publication__star" id="starOne_${publicationsToShow[i].id_post}" onclick="ratio(this.id,${publicationsToShow[i].id_post});" ></i>
+              <i class="far fa-star fa-2x publication__star" id="starTwo_${publicationsToShow[i].id_post}" onclick="ratio(this.id,${publicationsToShow[i].id_post});"></i>
+              <i class="far fa-star fa-2x publication__star" id="starThree_${publicationsToShow[i].id_post}" onclick="ratio(this.id,${publicationsToShow[i].id_post});"></i>
+              <i class="far fa-star fa-2x publication__star" id="starFour_${publicationsToShow[i].id_post}" onclick="ratio(this.id,${publicationsToShow[i].id_post});"></i>
+              <i class="far fa-star fa-2x publication__star" id="starFive_${publicationsToShow[i].id_post}" onclick="ratio(this.id,${publicationsToShow[i].id_post});"></i>
+          </div>
+          <div class="publication_comments container col-12">
+              <div class="publication_comments_create row">
+                  <div class="input-group mb-3">
+                      <input type="text" class="form-control" placeholder="Agregue un comentario" aria-label="Recipient's username" aria-describedby="basic-addon2" id='comentario_publicacion_${publicationsToShow[i].id_post}'>
+                      <div class="input-group-append">
+                          <button class="btn btn-outline-secondary" type="button" onclick="comentario(${publicationsToShow[i].id_post});">Comentar</button>
+                      </div>
+                  </div>
+
+              </div>
+              <div class="publication_comments__text row">
+                <div class="container comments__comment col-12">
+                  <div class="comments__data row" id="cargar_comentarios_${publicationsToShow[i].id_post}">
+
+                  </div>
+
+                </div>
+              </div>
+          </div>
+          <div>
+            <input value="${publicationsToShow[i].id_post}" type="hidden" readonly="readonly" class="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" id='id_publicacion_${publicationsToShow[i].id_post}'>
+          </div>
+      </div>`
+      }
+  });
+  });
+
+}
+
 
 function publicar() {
   var autor = document.formpublicacion.autor;
@@ -118,8 +214,8 @@ function publicar() {
 
 
 function comentario(id) {
-  var id_publicacion = document.getElementById("id_publicacion_"+id);
-  var comentario = document.getElementById("comentario_publicacion_"+id);
+  var id_publicacion = document.getElementById("id_publicacion_" + id);
+  var comentario = document.getElementById("comentario_publicacion_" + id);
   var usuario = verusuario();
 
 
